248c5946ad20e375594eee7114d08b95f6eddb1f
Riokyanpu <kgst1216@gmail.com>
2024-05-16 15:53:19 +0900

    値と文字列の違い

--- Main.java
+++ Main.java
@@ -1,5 +1,10 @@
+/* Javaの練習
+    ここまでがコメント*/ 
+   
 public class Main {
   public static void main(String[]arag) {
       System.out.println("Hollo world");
+      System.out.println(100 + 30);
+      System.out.println("100 + 30");
   }
 }
