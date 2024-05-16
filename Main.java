3ea7d525a06aa4165c07aae5ad34c89453904d4e
Riokyanpu <kgst1216@gmail.com>
2024-05-16 15:06:11 +0900

    Initial commit

---
+++ Main.java
@@ -0,0 +1,8 @@
+class Main {
+  public static void main(String[] args) {
+      String msg = "5月15日の勉強時間1時間";
+      // String msg = "Hello,World!";
+      // String mas = "5月15日の勉強時間1時間";
+      System.out.println(msg);
+  }
+}
