5be8c0181f7b2b8aac2856ffc001ddbcba2af062
Riokyanpu <kgst1216@gmail.com>
2024-05-16 15:19:12 +0900

    1回目

--- Main.java
+++ Main.java
@@ -1,8 +1,5 @@
-class Main {
-  public static void main(String[] args) {
-      String msg = "5月15日の勉強時間1時間";
-      // String msg = "Hello,World!";
-      // String mas = "5月15日の勉強時間1時間";
-      System.out.println(msg);
+public class Main {
+  public static void main(String[]arag) {
+      System.out.println("Hollo world");
   }
 }
