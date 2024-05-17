e93c24bb4a2330c452794e0fdb29027f47172a0b
Riokyanpu <kgst1216@gmail.com>
2024-05-17 14:23:31 +0900

    2回目

--- Main.java
+++ Main.java
@@ -4,7 +4,21 @@
 public class Main {
   public static void main(String[]arag) {
       System.out.println("Hollo world");
-      System.out.println(100 + 30);
-      System.out.println("100 + 30");
+      System.out.println(12345 + 6789);  //19134
+      System.out.println(12345 - 6789); //5556
+      System.out.println(12345 * 6789); //83810205
+      System.out.println(12345 / 5); //2469
+      System.out.println(12345 + 5 * 5); // 12370
+      System.out.println((12345 + 5) * 5); // 61750
+// 変数にデータを入れる
+      String greeting = "Hell world";
+      System.out.println(greeting); // Hello world
+      System.out.println(greeting); // Hello world
+      
+      int number = 100;
+      System.out.println(number); //100
+//文字列を受け取る
+
   }
 }
+
