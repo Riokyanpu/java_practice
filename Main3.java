138dd06b61ee17f724ab95ea7204f9aa49eeec3f
Riokyanpu <kgst1216@gmail.com>
2024-05-17 15:52:03 +0900

    条件に一致したら処理を実行

---
+++ Main3.java
@@ -0,0 +1,15 @@
+//条件に一致したら処理を実行する
+
+import java.util.*;
+
+public class Main3 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int number = scan.nextInt();
+        System.out.println(number);
+
+        if (number == 10)  {
+          System.out.println("Welcom");
+      }
+    }
+}
