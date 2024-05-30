f46769c6ad925e98eb9102cd83331c9c5df025f4
Riokyanpu <kgst1216@gmail.com>
2024-05-30 12:09:48 +0900

    条件2

--- Main5_1.java
+++ Main5_1.java
@@ -2,7 +2,7 @@
 
 import java.util.*;
 
-public class Main5 {
+public class Main5_1 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         String name = scan.next();
