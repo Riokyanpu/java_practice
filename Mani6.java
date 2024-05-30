fddd7e7d60983624600b721976140cbd339df967
Riokyanpu <kgst1216@gmail.com>
2024-05-30 12:23:52 +0900

    繰り返し処理

---
+++ Mani6.java
@@ -0,0 +1,11 @@
+public class Mani6 {
+  public static void main(String[] args) {
+    String greeting = "Hello paiza";
+  
+    for (int i = 0 ; i < 3; i++) 
+         System.out.println(greeting);  
+  }
+}
+
+/*for (int i = 0 ; i < 3; i++) 
+  変数iをカウンタ変数に 繰り返しの条件：i < 3  カウンタヘンスを増減：i++     */
