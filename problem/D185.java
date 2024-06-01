9a27164c9257a3a7436edfaf550137a64b4c7e1c
Riokyanpu <kgst1216@gmail.com>
2024-06-01 10:49:06 +0900

    計算プログラム

---
+++ problem/D185.java
@@ -0,0 +1,15 @@
+package problem;
+
+import java.util.*;
+public class D185 {
+    public static void main(String[] args) {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        Scanner sc = new Scanner(System.in);
+        int wage  = sc.nextInt();
+        int work  = sc.nextInt();
+        int total = wage * work ;
+        System.out.println(total);
+    }
+}
+
