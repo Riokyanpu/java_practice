4754459d8c2d9f5ec00afebc41a79547c25f1e14
Riokyanpu <kgst1216@gmail.com>
2024-06-25 09:26:33 +0900

    文字を受け取る

--- Main4.java
+++ Main4.java
@@ -5,6 +5,8 @@ import java.util.*;
 public class Main4 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        String text = scan.next();
+        System.out.println(text);
         int number = scan.nextInt();
         System.out.println(number);
         
