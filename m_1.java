3ee28d6983d78cb227d1118c97b1bb10b108d767
Riokyanpu <kgst1216@gmail.com>
2024-06-26 09:25:10 +0900

    HTMLを表示

---
+++ m_1.java
@@ -0,0 +1,8 @@
+// HTMLを表示する
+public class m_1 {
+	public static void main(String[] args) {
+		System.out.println("<h1>hello world</h1>");
+		System.out.println("<p>世界の皆さん");
+		System.out.println("<b>こんにちは</b></p>");
+	}
+}
