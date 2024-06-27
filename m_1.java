77793ebb93d2f68a23c0dd9225e098389b148c4a
Riokyanpu <kgst1216@gmail.com>
2024-06-27 09:16:38 +0900

    変数

--- m_1.java
+++ m_1.java
@@ -4,5 +4,12 @@ public class m_1 {
 		System.out.println("<h1>hello world</h1>");
 		System.out.println("<p>世界の皆さん");
 		System.out.println("<b>こんにちは</b></p>");
+    
+// 変数を使う
+    String player = "戦士";
+		System.out.println(player +"は、荒野を歩いていた");
+		System.out.println(player +"は、モンスターと戦った");
+		System.out.println(player +"は、モンスターをたおした");
+
 	}
 }
