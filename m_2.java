ff96fac3f598f27d270367a4f7968b1fe14b76a7
Riokyanpu <kgst1216@gmail.com>
2024-07-22 10:18:39 +0900

    IF 条件式サンプル

--- m_2.java
+++ m_2.java
@@ -1,3 +1,13 @@
 public class m_2 {
-  
+//01:IF文による条件分岐
+    public static void main(String[] args) {
+        int number = 2;
+        if (number == 1) {
+                  System.out.println("ok!");//条件式が成立した時の結果
+        } else {
+                System.out.println("on?");//条件式が成立しなかった時の結果
+                System.out.println("on?");
+        }
+    }
 }
+
