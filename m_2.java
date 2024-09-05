a3c42c16a1827127bae049293cdf1d432ec8095a
Riokyanpu <kgst1216@gmail.com>
2024-09-05 11:25:06 +0900

    比較演算その7-3

--- m_2.java
+++ m_2.java
@@ -89,6 +89,13 @@ public class m_2 {
 	      } else {
           System.out.println("クリティカルヒット！スライムに、100のダメージを与えた！");
         } 
+        int add = (int)(Math.random() * 10 + 1); // この行を修正して1から2のサイコロになるようにする
+        System.out.println("追加のサイコロは、" + add);
+        if (add == 1) { // この行の条件式を修正
+          System.out.println("追加攻撃!スライムに、10のダメージを与えた!!!");
+        } else {
+          System.out.println("追加攻撃に失敗!");
+        }
         
         }
     }
