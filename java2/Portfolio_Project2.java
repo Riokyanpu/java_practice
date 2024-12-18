5184c2edcf984714bab01f4fdabbfbad40c275dc
Riokyanpu <kgst1216@gmail.com>
2024-12-18 13:50:15 +0900

    chapter5-2

--- java2/Portfolio_Project2.java
+++ java2/Portfolio_Project2.java
@@ -4,6 +4,7 @@ public class Portfolio_Project2 {
       runChapter2();  // Chapter 2のメソッド呼び出し
       runChapter3();  // Chapter 3のメソッド呼び出し
       runChapter4();  // Chapter 4のメソッド呼び出し
+      runChapter5();  // Chapter 5のメソッド呼び出し
 
     }
      
@@ -47,5 +48,33 @@ public class Portfolio_Project2 {
 		} else {
 			System.out.println("omikujiの中身は" + omikuji + "なので大凶");
 		}
+    }
+//05:RPGのクリティカルヒットを再現
+// RPGのクリティカルヒットを再現
+// 比較演算子 == > < >= <= !=
+
+
+// スライムと戦っている。
+// 1から10のサイコロをふって、
+// 6未満：サイコロの目だけダメージを与えたと表示。
+// 6以上：クリティカルヒットとして、100のダメージを与えたと表示。
+// さらに、1から2のサイコロをふって、
+// 1：追加攻撃として、10のダメージを与えたと表示。
+// 2：追加攻撃に失敗したと表示。
+    public static void runChapter5()  {
+        int hit = (int)(Math.random() * 10 + 1);
+        if (hit < 6) {
+            System.out.println("スライムに" + hit + "のダメージを与えた");
+        } else {
+            System.out.println("クリティカルヒット!スライムに、100のダメージを与えた!!");
+        }
+
+        int add = (int)(Math.random() * 2 + 1); // この行を修正して1から2のサイコロになるようにする
+        System.out.println("追加のサイコロは、" + add);
+        if (add == 1) { // この行の条件式を修正
+            System.out.println("追加攻撃!スライムに、10のダメージを与えた!!!");
+        } else {
+            System.out.println("追加攻撃に失敗!");
+        }
     }
 }
